576e2716e9c3717c84aa551733101558077ac84f
Evan George <evandgeorge@gmail.com>
2019-11-04 20:42:03 -0500

    Added Shape abstract

---
+++ src/geometry/Shape.h
@@ -0,0 +1,25 @@
+//
+// Created by Evan George on 11/4/19.
+//
+
+#ifndef RAYTRACERCPP_SHAPE_H
+#define RAYTRACERCPP_SHAPE_H
+
+#include "Color.h"
+#include "Vector3D.h"
+#include "Ray.h"
+
+namespace raytracer {
+	class Shape {
+	public:
+		Shape(const Color &c, double r) : color(c), reflectivity(r) {}
+
+		//abstract method to be implemented by subclasses of shape that find the time of the first intersection between the ray and the shape
+		virtual double findIntersection(const Ray &ray) = 0;
+	private:
+		Color color;
+		double reflectivity;	//Proportion of light that is reflected, complement of proportion of light that is scattered (matte reflectance)
+	};
+}
+
+#endif //RAYTRACERCPP_SHAPE_H
