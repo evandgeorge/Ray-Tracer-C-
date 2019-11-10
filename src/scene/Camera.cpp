cd0345840283fe82761a063aed64dc38b9501a06
Evan George <evandgeorge@gmail.com>
2019-11-10 15:24:22 -0500

    Added updateCameraRays to create the cameraRays for each pixel, and various setter functions for the properties of the camera

--- src/scene/Camera.cpp
+++ src/scene/Camera.cpp
@@ -5,26 +5,117 @@
 #include "Camera.h"
 #include <cmath>
 
-void raytracer::Camera::updateCornerPositions() {
-	double aspectRatio = (double) renderWidth / renderHeight;;
-	spacialWidth = spacialHeight * aspectRatio;
-	spacialDepth = aspectRatio / tan(hFOV / 2);
+//set the origin to v
+void raytracer::Camera::moveTo(const raytracer::Vector3D &v) {
+	origin = v;
+	updateCamera();
+}
 
+//move the camera by v
+void raytracer::Camera::translate(const raytracer::Vector3D &v) {
+	origin = origin + v;
+	updateCamera();
+}
+
+//set the azimuth angle of the camera
+void raytracer::Camera::set_xy_rotation(double azimuth) {
+	xy_rotation = azimuth;
+	updateCamera();
+}
+
+//increment the xy_rotation by angle
+void raytracer::Camera::turn(double angle) {
+	xy_rotation += angle;
+	updateCamera();
+}
+
+//set the inclination angle of the camera
+void raytracer::Camera::set_z_rotation(double inclination) {
+	z_rotation = inclination;
+	updateCamera();
+}
+
+//increment the z_rotation by angle
+void raytracer::Camera::incline(double angle) {
+	z_rotation += angle;
+	updateCamera();
+}
+
+//reset the axis rotation of the camera to 0
+void raytracer::Camera::resetRotation() {
+	roll = 0;
+	updateCamera();
+}
+
+//rotate the camera by an additional amount about its axis
+void raytracer::Camera::rotate(double rotation) {
+	roll += rotation;
+	updateCamera();
+}
+
+//set the resolution of the render image
+void raytracer::Camera::setResolution(int w, int h) {
+	renderWidth = w;
+	renderHeight = h;
+	updateCamera();
+}
+
+//updates the positions of the corners of the viewport
+void raytracer::Camera::updateCornerPositions() {
+	double aspectRatio = (double) renderWidth / renderHeight;
+	spacialWidth = spacialHeight * aspectRatio;					//update the width of the viewport in the scene
+	spacialDepth = aspectRatio / tan(hFOV / 2);					//update the height of the viewport in the scene
+
+	//the positive difference in inclination (z) angle between the top or bottom sides of the viewport and the center axis
 	double inclinationFromMiddle = atan(sqrt(1 + pow(aspectRatio, 2)) / spacialDepth);
+	//the positive difference in azimuth (xy) angle between the left or right side of the viewport and the center axis
 	double azimuthFromMiddle = atan(1 / aspectRatio);
 
+	//the distance from the viewport corners to the origin of the camera
 	double radius = sqrt(pow(spacialHeight / 2, 2) + pow(spacialWidth / 2, 2) + pow(spacialDepth, 2));
 
+	//the corners of the viewport relative to the origin of the camera
 	Vector3D cornersRelativeToCameraOrigin[4] = {
-		sphericalToCartesian(radius, z_rotation - inclinationFromMiddle, xy_rotation - azimuthFromMiddle),
-		sphericalToCartesian(radius, z_rotation - inclinationFromMiddle, xy_rotation + azimuthFromMiddle),
-		sphericalToCartesian(radius, z_rotation + inclinationFromMiddle, xy_rotation + azimuthFromMiddle),
-		sphericalToCartesian(radius, z_rotation + inclinationFromMiddle, xy_rotation - azimuthFromMiddle)};
+			sphericalToCartesian(radius, z_rotation - inclinationFromMiddle, xy_rotation + azimuthFromMiddle),
+			sphericalToCartesian(radius, z_rotation - inclinationFromMiddle, xy_rotation - azimuthFromMiddle),
+			sphericalToCartesian(radius, z_rotation + inclinationFromMiddle, xy_rotation + azimuthFromMiddle),
+			sphericalToCartesian(radius, z_rotation + inclinationFromMiddle, xy_rotation - azimuthFromMiddle)};
 
+	//direction vector describing the axis of rotation for the viewport corners, the line from the origin through the center of the camera
 	Vector3D rotationAxis = sphericalToCartesian(1, z_rotation, xy_rotation);
 
+	//the absolute positions of the pre-rotation viewport corners in the scene
 	topLeftCorner = origin + cornersRelativeToCameraOrigin[0].rotatedAbout(rotationAxis, roll);
 	topRightCorner = origin + cornersRelativeToCameraOrigin[1].rotatedAbout(rotationAxis, roll);
 	bottomLeftCorner = origin + cornersRelativeToCameraOrigin[2].rotatedAbout(rotationAxis, roll);
 	bottomRightCorner = origin + cornersRelativeToCameraOrigin[3].rotatedAbout(rotationAxis, roll);
+}
+
+//update the camera rays
+void raytracer::Camera::updateCameraRays() {
+	pixelCoordinateMap pixelCenters = getPixelCenters();
+
+	//reinitialize cameraRays to clear it
+	cameraRays = pixelCameraRayMap(renderWidth);
+
+	//for each pixel in the viewport create a ray that starts at the camera origin and passes through the center of the pixel
+	for(int w = 0; w < renderWidth; ++w)
+		for(int h = 0; h < renderHeight; ++h)
+			cameraRays[w].push_back(Ray(origin, pixelCenters[w][h] - origin));
+
+}
+
+//return a two dimensional vector of the center of each pixel on the viewport represented by a Vector3D
+raytracer::Camera::pixelCoordinateMap raytracer::Camera::getPixelCenters() const {
+	Vector3D heightUnit = (topLeftCorner - bottomLeftCorner) / renderHeight;	//height of one pixel
+	Vector3D widthUnit = (topLeftCorner - topRightCorner) / renderWidth;		//width of one pixel
+
+	pixelCoordinateMap pixelCenters(renderWidth);
+
+	//for each pixel in the viewport add a Vector3D that represents the position of its center
+	for(int x = 0; x < renderWidth; ++x)
+		for(int y = 0; y < renderHeight; ++y)
+			pixelCenters[x].push_back(Vector3D(bottomRightCorner + widthUnit * (x + .5) + heightUnit * (y + .5)));
+
+	return pixelCenters;
 }
--- src/scene/Camera.h
+++ src/scene/Camera.h
@@ -5,9 +5,9 @@
 #ifndef RAYTRACERCPP_CAMERA_H
 #define RAYTRACERCPP_CAMERA_H
 
-
 #include <geometry/Vector3D.h>
-#include <cmath>
+#include <geometry/Ray.h>
+#include <vector>
 
 namespace raytracer {
 
@@ -17,10 +17,37 @@ namespace raytracer {
 				: renderWidth(width), renderHeight(height), origin(origin), hFOV(hFOV), xy_rotation(xy_rotation), z_rotation(z_rotation), roll(roll) {
 
 			updateCornerPositions();
+			updateCameraRays();
 		}
 
+		void moveTo(const Vector3D &v);			//set the origin to v
+		void translate(const Vector3D &v);		//move the camera by v
+
+		void set_xy_rotation(double azimuth);	//set the azimuth angle of the camera
+		void turn(double angle);				//increment the xy_rotation by angle
+
+		void set_z_rotation(double inclination);//set the inclination angle of the camera
+		void incline(double angle);				//increment the z_rotation by angle
+
+		void resetRotation();					//reset the axis rotation of the camera to 0
+		void rotate(double rotation);			//rotate the camera by an additional amount about its axis
+
+		void setResolution(int w, int h);		//set the resolution of the render image
+
 	private:
-		void updateCornerPositions();
+		//two dimensional vector of Vector3Ds, used for storing the position of the center of each pixel on the viewport
+		typedef std::vector<std::vector<Vector3D>> pixelCoordinateMap;
+		//two dimensional vector of Rays, used for storing the camera ray for each pixel
+		typedef std::vector<std::vector<Ray>> pixelCameraRayMap;
+
+		//updates the corner vectors and the camera rays
+		void updateCamera() { updateCornerPositions(); updateCameraRays(); }
+
+		void updateCornerPositions();			//updates the positions of the corners of the viewport
+		void updateCameraRays();				//updates the camera rays
+
+		//return a two dimensional vector of the center of each pixel on the viewport represented by a Vector3D
+		pixelCoordinateMap getPixelCenters() const;
 
 		//GIVEN VALUES
 		int renderWidth, renderHeight;			//width and height in pixels of rendered scene with camera
@@ -29,7 +56,7 @@ namespace raytracer {
 		double xy_rotation, z_rotation;			//spherical coordinates of center of camera viewport ind 3D space, xy_rotation (azimuth), z_rotation(inclination)
 		double roll;							//rotation of camera about the axis drawn from the origin to the center of the viewport
 
-		const double spacialHeight = 0;			//height of camera viewport in 3D space, fixed to arbitrary 2 units tall
+		const double spacialHeight = 2;			//height of camera viewport in 3D space, fixed to arbitrary 2 units tall
 
 		//DEPENDENT VALUES
 		double spacialWidth;					//width of camera viewport in 3D space, dependent on the aspect ratio and spacialHeight (constant)
@@ -39,6 +66,8 @@ namespace raytracer {
 					topRightCorner,
 					bottomLeftCorner,
 					bottomRightCorner;
+
+		pixelCameraRayMap cameraRays;
 	};
 }
 
