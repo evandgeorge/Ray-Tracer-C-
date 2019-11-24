dae662a8a3f17383a424db482e9f181330c9ae94
Evan George <evandgeorge@gmail.com>
2019-11-23 21:53:12 -0500

    Reformatted Vector3D.cpp

--- src/geometry/Vector3D.cpp
+++ src/geometry/Vector3D.cpp
@@ -5,78 +5,82 @@
 #include <cmath>
 #include "Vector3D.h"
 
-//returns the magnitude of the vector
-double raytracer::Vector3D::magnitude() const {
-	return sqrt(x*x + y*y + z*z);
-}
+namespace raytracer {
 
-//returns the dot product of *this and v
-double raytracer::Vector3D::dotProduct(const Vector3D &v) const {
-	return (x * v.x) + (y * v.y) + (z * v.z);
-}
+    //returns the magnitude of the vector
+    double Vector3D::magnitude() const {
+        return sqrt(x * x + y * y + z * z);
+    }
 
-//returns *this cross v
-raytracer::Vector3D raytracer::Vector3D::crossProduct(const Vector3D &v) const {
-	double x_cross = y * v.z - z * v.y;
-	double y_cross = z * v.x - x * v.z;
-	double z_cross = x * v.y - y * v.x;
+    //returns the dot product of *this and v
+    double Vector3D::dotProduct(const Vector3D &v) const {
+        return (x * v.x) + (y * v.y) + (z * v.z);
+    }
 
-	return Vector3D(x_cross, y_cross, z_cross);
-}
+    //returns *this cross v
+    Vector3D Vector3D::crossProduct(const Vector3D &v) const {
+        double x_cross = y * v.z - z * v.y;
+        double y_cross = z * v.x - x * v.z;
+        double z_cross = x * v.y - y * v.x;
 
-//returns true if the vectors are exactly identical
-bool raytracer::Vector3D::operator==(const raytracer::Vector3D &v) const {
-	return (x == v.x) && (y == v.y) && (z == v.z);
-}
+        return {x_cross, y_cross, z_cross};
+    }
 
-//returns *this + v
-raytracer::Vector3D raytracer::Vector3D::operator+(const raytracer::Vector3D &v) const {
-	return {x + v.x,
-		y + v.y,
-		z + v.z};
-}
+    //returns true if the vectors are exactly identical
+    bool Vector3D::operator==(const Vector3D &v) const {
+        return (x == v.x) && (y == v.y) && (z == v.z);
+    }
 
-//returns *this - v
-raytracer::Vector3D raytracer::Vector3D::operator-(const raytracer::Vector3D &v) const {
-	return {x - v.x,
-		y - v.y,
-		z - v.z};
-}
+    //returns *this + v
+    Vector3D Vector3D::operator+(const Vector3D &v) const {
+        return {x + v.x,
+                y + v.y,
+                z + v.z};
+    }
 
-//returns *this * f (scalar)
-raytracer::Vector3D raytracer::Vector3D::operator*(double f) const {
-	return {x * f,
-		y * f,
-		z * f};
-}
+    //returns *this - v
+    Vector3D Vector3D::operator-(const Vector3D &v) const {
+        return {x - v.x,
+                y - v.y,
+                z - v.z};
+    }
 
-//returns *this / f (scalar)
-raytracer::Vector3D raytracer::Vector3D::operator/(double d) const {
-	return {x / d,
-		y / d,
-		z / d};
-}
+    //returns *this * f (scalar)
+    Vector3D Vector3D::operator*(double f) const {
+        return {x * f,
+                y * f,
+                z * f};
+    }
 
-//returns -(*this)
-raytracer::Vector3D raytracer::Vector3D::operator-() const {
-	return {-x,
-		-y,
-		-z};
-}
+    //returns *this / f (scalar)
+    Vector3D Vector3D::operator/(double d) const {
+        return {x / d,
+                y / d,
+                z / d};
+    }
 
-//returns *this rotated about the rotation axis described by v. It is not required that v is normalized because its normal is used for calculations.
-raytracer::Vector3D raytracer::Vector3D::rotatedAbout(const raytracer::Vector3D &v, double angle) const {
-	Vector3D axisNormal = getNormalizedVector(v);
-	return *this * cos(angle) + (axisNormal.crossProduct(*this) * sin(angle) + axisNormal * (axisNormal.dotProduct(*this)) * (1 - cos(angle)));
-}
+    //returns -(*this)
+    Vector3D Vector3D::operator-() const {
+        return {-x,
+                -y,
+                -z};
+    }
 
-//returns a Vector3D in the direction of (x, y, z) with size 1
-raytracer::Vector3D raytracer::getNormalizedVector(const Vector3D &v) {
-	return v / v.magnitude();
-}
+    //returns *this rotated about the rotation axis described by v. It is not required that v is normalized because its normal is used for calculations.
+    Vector3D Vector3D::rotatedAbout(const Vector3D &v, double angle) const {
+        Vector3D axisNormal = getNormalizedVector(v);
+        return *this * cos(angle) + (axisNormal.crossProduct(*this) * sin(angle) +
+                                     axisNormal * (axisNormal.dotProduct(*this)) * (1 - cos(angle)));
+    }
 
-raytracer::Vector3D raytracer::sphericalToCartesian(double r, double theta, double phi) {
-	return {r * sin(theta) * cos(phi),
-		 r * sin(theta) * sin(phi),
-		 r * cos(theta)};
+    //returns a Vector3D in the direction of (x, y, z) with size 1
+    Vector3D getNormalizedVector(const Vector3D &v) {
+        return v / v.magnitude();
+    }
+
+    Vector3D sphericalToCartesian(double r, double theta, double phi) {
+        return {r * sin(theta) * cos(phi),
+                r * sin(theta) * sin(phi),
+                r * cos(theta)};
+    }
 }
